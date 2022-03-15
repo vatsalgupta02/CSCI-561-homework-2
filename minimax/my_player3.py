91390b182af2719c257807ff053e183e1bd6db0a
vatsalgupta02 <vatsalgupta02@outlook.com>
2022-03-15 00:56:54 -0400

    added the evaluation method for minimax

--- minimax/my_player3.py
+++ minimax/my_player3.py
@@ -5,22 +5,48 @@ from write import writeOutput
 
 from host import GO
 
-class RandomPlayer():
-    def __init__(self):
+class MiniMax():
+    def __init__(self, piece_type):
+        self.piece_type = piece_type
         self.type = 'random'
 
-    def get_input(self, go, piece_type):
+    def isMovesLeft(self, board):
+        # game has not ended, moves may still be present
+        if not board.game_end(self.piece_type):
+            return True
+
+    def evaluate(self, b):
+        _min, _max, _eval_min, _eval_max = 0, 0, 0, 0
+        for i in range(5):
+            for j in range(5):
+                if b[i][j] == self.piece_type:
+                    _max += 1
+                    _eval_max += (_max + b.find_liberty(i, j))
+                elif b[i][j] == 3-self.piece_type:
+                    _min += 1
+                    _eval_min += (_min + b.find_liberty(i, j))
+        heuristic = _eval_max - _eval_min
+        # if self.piece_type == player:
+        #     heuristic = _eval_max - _eval_min
+        # else:
+        #     heuristic = _eval_min - _eval_max
+        return heuristic
+
+    def minimax(self, board, depth, isMax):
+        score = self.evaluate(board)
+        pass
+
+    def move(self, go):
         '''
         Get one input.
 
         :param go: Go instance.
-        :param piece_type: 1('X') or 2('O').
         :return: (row, column) coordinate of input.
         '''        
         possible_placements = []
         for i in range(go.size):
             for j in range(go.size):
-                if go.valid_place_check(i, j, piece_type, test_check = True):
+                if go.valid_place_check(i, j, self.piece_type, test_check = True):
                     possible_placements.append((i,j))
 
         if not possible_placements:
@@ -33,6 +59,6 @@ if __name__ == "__main__":
     piece_type, previous_board, board = readInput(N)
     go = GO(N)
     go.set_board(piece_type, previous_board, board)
-    player = RandomPlayer()
-    action = player.get_input(go, piece_type)
+    player = MiniMax(piece_type)
+    action = player.move(go)
     writeOutput(action)
